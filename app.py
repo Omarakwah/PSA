5436d8086e9a56aa7395ebea00a80812fc20e5c5
Omar Akwah <omarakwah@users.noreply.huggingface.co>
2024-05-03 02:20:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ from huggingface_hub import HfApi
 sys.path.append('../..')
 
 
-openai.api_key  = os.getenv["OPENAI_API_KEY"]
-api_key=os.getenv["api_key"]
+openai.api_key  = os.environ.get("OPENAI_API_KEY")
+api_key=os.environ.get("api_key")
 llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
-google_api=os.getenv["GOOGLE_API_KEY"] 
-CS_API= os.getenv["GOOGLE_CSE_ID"] 
+google_api=os.environ.get("GOOGLE_API_KEY") 
+CS_API= os.environ.get("GOOGLE_CSE_ID")  
 
 
 RESULTS_PER_QUESTION = 3
